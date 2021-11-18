13d53384dcb6cc68e3790d60aff9a38990fc0f00
Aidan Urschel <aidanjinkyoo@mail.com>
2021-11-18 17:37:52 -0500

    Final Commit Redo

---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+const api = require('./routes/routes');
+
+// Setting port
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// static assets
+app.use(express.static('public'));
+
+app.use('/api', api);
+
+// set the routes
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+app.listen(PORT, () => 
+console.log(`Listening on PORT: ${PORT}`)
+);
